26a1ed55b8c995d1c6db93a98087ccb05701cb87
Sachin-1-9 <kit27.am42@gmail.com>
2025-06-18 09:13:03 +0530

    Add files via upload

---
+++ day12/task2.js
@@ -0,0 +1,3 @@
+let a=prompt("enter an text");
+var d=a.length;
+alert("you have used: "+d+" remaining:"+(200-d)+" char")
---
+++ day12/task4.js
@@ -0,0 +1,6 @@
+let a=10;
+let b=5;
+console.log(`ADDITION: ${a+b}`);
+console.log(`SUBRACTION: ${a-b}`);
+console.log(`MULTIPLICATION: ${a*b}`);
+console.log(`DIVISION: ${a/b}`);
